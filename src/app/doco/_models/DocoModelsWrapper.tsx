0375271d056a8ec4204bb2831d8fcdd0e58968e9
yotamos5699 <m.lhafakot@gmail.com>
2025-03-30 16:29:58 +0300

    as

--- src/app/doco/_models/DocoModelsWrapper.tsx
+++ src/app/doco/_models/DocoModelsWrapper.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { useShallow } from "zustand/react/shallow";
 import { useAttachmentsStore } from "../_stores/attachments_stores/useAttachmentsStore";
-import { NewGenericModelContainer } from "@/_components/GenericModelContainer";
+import { GenericModelContainer } from "@/_components/GenericModelContainer";
 
 export const DocoModelsWrapper = () => {
   const md = useAttachmentsStore((state) => state.md);
   const setMd = useAttachmentsStore((state) => state.setMd);
-  return <NewGenericModelContainer md={md} setMd={setMd} className={md?.css} />;
+  return <GenericModelContainer md={md} setMd={setMd} className={md?.css} />;
 };
